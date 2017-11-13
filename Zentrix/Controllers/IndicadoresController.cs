7a76dd53de8de6a7eb5a8066c9ad4968b32f355a
juan <juan@DESKTOP-TPMRT8V>
2017-11-13 17:04:24 -0400

    Se agrega varias vistas parciales

---
+++ Zentrix/Controllers/IndicadoresController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zentrix.Models.BD;
+
+namespace Zentrix.Controllers
+{
+    public class IndicadoresController : Controller
+    {
+        private BDEntities db = new BDEntities();
+        // GET: Indicadores
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DetailFincanciera() {
+            return PartialView(db.Indicador.ToList());
+        }
+        public ActionResult DetailCliente()
+        {
+            return PartialView(db.Indicador.ToList());
+        }
+        public ActionResult DetailInterna()
+        {
+            return PartialView(db.Indicador.ToList());
+        }
+        public ActionResult DetailAprendizaje()
+        {
+            return PartialView(db.Indicador.ToList());
+        }
+    }
+}
